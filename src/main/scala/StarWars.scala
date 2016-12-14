b00fa5994ce977a483516da7e65bb19269359de6
k--kato <keisuke.k.kato@gmail.com>
2016-12-14 09:26:17 +0900

    fix type

--- src/main/scala/StarWars.scala
+++ src/main/scala/StarWars.scala
@@ -2,7 +2,5 @@ import scala.Mutable
 import scala.Array
 
 object StarWars extends App {
-  List(1,2)
-  val say = (s: String) => println(s)
-  say("Not if anything to say about it I have");
+  println("Not if anything to say about it I have")
 }
